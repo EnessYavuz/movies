db0aca47cc798ba83794e46069a6ee088a06182b
Enes Yavuz <enesyavuz921@gmail.com>
2023-10-18 13:48:08 +0300

    api study-1

--- src/App.js
+++ src/App.js
@@ -1,80 +1,10 @@
 import CartList from "./components/CartList";
 import InputMovie from "./components/InputMovie";
-import { useState } from "react";
-import * as images from "./components/Images";
+import { useEffect, useState } from "react";
+// import * as images from "./components/Images";
 import MesajBox from "./components/MesajBox";
+import axios from "axios";
 
-const movies = [
-  {
-    id: 1,
-    title: "BATMAN",
-    image: images.jokerImage,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-    IMDB: 7.0,
-  },
-  {
-    id: 2,
-    title: "SUPERMAN",
-    image: images.jokerImage,
-
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-    IMDB: 7.0,
-  },
-  {
-    id: 3,
-    title: "Ninja Turtles",
-    image: images.jokerImage,
-
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-    IMDB: 7.0,
-  },
-  {
-    id: 4,
-    title: "Love And Mosters",
-    image: images.jokerImage,
-
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-    IMDB: 7.0,
-  },
-  {
-    id: 5,
-    title: "Harry Potter",
-    image: images.jokerImage,
-
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-    IMDB: 7.0,
-  },
-  {
-    id: 6,
-    title: "Lord of Rings",
-    image: images.jokerImage,
-
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-    IMDB: 7.0,
-  },
-  {
-    id: 7,
-    title: "Joker",
-    image: images.jokerImage,
-
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-    IMDB: 7.0,
-  },
-  {
-    id: 8,
-    title: "Fury",
-    image: images.jokerImage,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-    IMDB: 7.0,
-  },
-  {
-    id: 9,
-    title: "Fight Club",
-    image: images.jokerImage,
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-    IMDB: 7.0,
-  },
-];
 
 const commentList =[
   {id:1,
@@ -88,14 +18,30 @@ const commentList =[
 function App() {
   const [search, setSearch] = useState("");
   const [comments, setComments] = useState(commentList)
+  const [movies, setMovies] = useState([])
 
   const onChangeInput = (event) => {
     setSearch(event.target.value);
   };
 
+  const gelenVeriler = async () => {
+    
+      const getResponse = await axios.get("http://localhost:3004/Movies");
+      setMovies(getResponse.data);
+
+  };
+
+  
+  useEffect(()=>{
+
+    gelenVeriler();
+
+  },[])
+
   const filteredMovies = movies.filter((movie) => {
     return movie.title.toLowerCase().indexOf(search.toLowerCase()) !== -1;
   });
+
 
 const handleMessage =(name,email,description)=>{
 
@@ -111,8 +57,12 @@ const comeData= [
 setComments(comeData)
 }
 
+
+
   return (
     <div className="App">
+
+
       <div className="inputContainer">
         <InputMovie handleInput={onChangeInput} />
       </div>
