75ab102e424199ba8f08813b5665b878ad39bb09
Enes Yavuz <enesyavuz921@gmail.com>
2023-10-19 10:42:54 +0300

    çalışan hali

---
+++ src/Home.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import CartList from "./components/CartList";
+import InputMovie from "./components/InputMovie";
+import { useEffect, useState } from "react";
+// import * as images from "./components/Images";
+import MesajBox from "./components/MesajBox";
+import axios from "axios";
+
+function Home() {
+  const [search, setSearch] = useState("");
+  const [comments, setComments] = useState([]);
+  const [movies, setMovies] = useState([]);
+
+  const onChangeInput = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const gelenVeriler = async () => {
+    const getResponse = await axios.get("http://localhost:3004/Movies");
+    setMovies(getResponse.data);
+  };
+
+  useEffect(() => {
+    gelenVeriler();
+  }, []);
+
+  const filteredMovies = movies.filter((movie) => {
+    return movie.title.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+  });
+
+  const handleMessage = async (name, email, description) => {
+    const response = await axios.post("http://localhost:3004/Comment", {
+      name,
+      email,
+      description,
+    });
+
+    const comeData = [...comments, response.data];
+    setComments(comeData);
+  };
+
+  const mjVerileri = async () => {
+    const response = await axios.get("http://localhost:3004/Comment");
+    setComments(response.data);
+  };
+
+  useEffect(() => {
+    mjVerileri();
+  }, []); // Boş bağımlılık dizisi verilerek sadece bileşen yüklendiğinde çağrılması sağlanır
+
+  return (
+    <div className="App">
+            <div className="inputContainer">
+        <InputMovie handleInput={onChangeInput} />
+      </div>
+      <div className="Container">
+        <div className="space">Boşluk</div>
+
+        <CartList movies={filteredMovies} />
+
+        <div className="space">Boşluk</div>
+      </div>
+
+      <div className="MesajContainer">
+        <div className="MjTitle">
+          <h1> Yorum Atın Amk</h1>
+        </div>
+        <div className="MainMj">
+          <MesajBox onCreateMessage={handleMessage} comments={comments} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,7 +5,7 @@ function Cart({ movie }) {
    
     <div className="Cart">
       <div className="imageContainer">
-      {/* <img src={movie.image} alt="resim" /> */}
+      <img src={movie.image} alt="resim" /> 
       </div>
       <div>
       <h2>{movie.title}</h2>
